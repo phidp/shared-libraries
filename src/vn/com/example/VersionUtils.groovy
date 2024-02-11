111b6cddb016047bfd6dacc3ab853f62d1579fb5
phidp <91424769+phidp@users.noreply.github.com>
2024-02-11 16:53:30 +0000

    update

---
+++ src/vn/com/example/VersionUtils.groovy
@@ -0,0 +1,24 @@
+//VersionUtils.groovy
+package vn.com.example
+
+class VersionUtils {
+
+    static def getMavenAndJavaVersion() {
+        def process = "mvn --version".execute()
+        process.waitFor()
+
+        def mavenVersionOutput = process.text.trim()
+        def mavenVersion = mavenVersionOutput.split('\n').find { it.startsWith('Apache Maven') }?.split(' ')[2]
+        def javaVersion = mavenVersionOutput.split('\n').find { it.startsWith('Java version') }?.split(' ')[2]
+
+        return [mavenVersion, javaVersion]
+    }
+
+    static void main(String[] args) {
+        def (mavenVersion, javaVersion) = getMavenAndJavaVersion()
+        println "||==========MAVEN VERSION==========||"
+        println "|| Apache Maven: $mavenVersion"
+        println "|| Java version: $javaVersion"
+        println "||=================================||"
+    }
+}
---
+++ vars/showMavenVersion.groovy
@@ -0,0 +1,4 @@
+def call() {
+    println('Retrieving Maven information')
+    VersionUtils.getMavenAndJavaVersion()
+}
