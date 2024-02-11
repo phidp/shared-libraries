30e3f4a55e242e8b26a260e4433d2fbe13bf4739
phidp <91424769+phidp@users.noreply.github.com>
2024-02-11 17:34:23 +0000

    test

--- src/vn/com/example/VersionUtils.groovy
+++ src/vn/com/example/VersionUtils.groovy
@@ -3,22 +3,22 @@ package vn.com.example
 
 class VersionUtils {
 
-    static def getMavenAndJavaVersion() {
-        def process = "/opt/homebrew/bin/mvn --version".execute()
-        process.waitFor()
+    static def getMavenAndJavaVersion(script) {
+        // def process = "/opt/homebrew/bin/mvn --version".execute()
+        // process.waitFor()
 
-        def mavenVersionOutput = process.text.trim()
+        def mavenVersionOutput = script.sh(script: '/opt/homebrew/bin/mvn --version', returnStdout: true).trim()
         def mavenVersion = mavenVersionOutput.split('\n').find { it.startsWith('Apache Maven') }?.split(' ')[2]
         def javaVersion = mavenVersionOutput.split('\n').find { it.startsWith('Java version') }?.split(' ')[2]
 
         return [mavenVersion, javaVersion]
     }
 
-    static void main(String[] args) {
-        def (mavenVersion, javaVersion) = getMavenAndJavaVersion()
-        println "||==========MAVEN VERSION==========||"
-        println "|| Apache Maven: $mavenVersion"
-        println "|| Java version: $javaVersion"
-        println "||=================================||"
-    }
+    // static void main(String[] args) {
+    //     def (mavenVersion, javaVersion) = getMavenAndJavaVersion()
+    //     println "||==========MAVEN VERSION==========||"
+    //     println "|| Apache Maven: $mavenVersion"
+    //     println "|| Java version: $javaVersion"
+    //     println "||=================================||"
+    // }
 }
--- vars/showMavenVersion.groovy
+++ vars/showMavenVersion.groovy
@@ -1,5 +1,9 @@
 import vn.com.example.VersionUtils
 def call() {
     println('Retrieving Maven information')
-    VersionUtils.getMavenAndJavaVersion()
+    def (mavenVersion, javaVersion) = VersionUtils.getMavenAndJavaVersion(this)
+    println "||==========MAVEN VERSION==========||"
+    println "|| Apache Maven: $mavenVersion"
+    println "|| Java version: $javaVersion"
+    println "||=================================||"
 }
