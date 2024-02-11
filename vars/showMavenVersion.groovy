53044b7b0f0f8f2364df40959dbd99d7e2f3fd86
phidp <91424769+phidp@users.noreply.github.com>
2024-02-11 17:38:36 +0000

    test

--- vars/showMavenVersion.groovy
+++ vars/showMavenVersion.groovy
@@ -2,8 +2,14 @@ import vn.com.example.VersionUtils
 def call() {
     println('Retrieving Maven information')
     def (mavenVersion, javaVersion) = VersionUtils.getMavenAndJavaVersion(this)
-    println "||==========MAVEN VERSION==========||"
-    println "|| Apache Maven: $mavenVersion"
-    println "|| Java version: $javaVersion"
-    println "||=================================||"
+    sh """
+        ||==========MAVEN VERSION==========||
+        || Apache Maven: ${mavenVersion}
+        || Java version: ${javaVersion}
+        ||=================================||
+    """
+    // println "||==========MAVEN VERSION==========||"
+    // println "|| Apache Maven: $mavenVersion"
+    // println "|| Java version: $javaVersion"
+    // println "||=================================||"
 }
