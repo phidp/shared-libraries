740ebece0e24097809df16ba086c5fa463b043d9
phidp <91424769+phidp@users.noreply.github.com>
2024-02-13 17:47:57 +0000

    add

--- src/vn/com/example/VersionUtils.groovy
+++ src/vn/com/example/VersionUtils.groovy
@@ -7,18 +7,16 @@ class VersionUtils {
         // def process = "/opt/homebrew/bin/mvn --version".execute()
         // process.waitFor()
 
-        def mavenVersionOutput = script.sh(script: 'mvn --version', returnStdout: true).trim()
+        def mavenVersionOutput = script.sh(script: '/usr/bin/mvn --version', returnStdout: true).trim()
         def mavenVersion = mavenVersionOutput.split('\n').find { it.startsWith('Apache Maven') }?.split(' ')[2]
         def javaVersion = mavenVersionOutput.split('\n').find { it.startsWith('Java version') }?.split(' ')[2]
 
-        return [mavenVersion, javaVersion]
+        result = """
+||==========MAVEN VERSION==========||
+|| Apache Maven: ${mavenVersion}
+|| Java version: ${javaVersion}
+||=================================||
+"""
+        return result
     }
-
-    // static void main(String[] args) {
-    //     def (mavenVersion, javaVersion) = getMavenAndJavaVersion()
-    //     println "||==========MAVEN VERSION==========||"
-    //     println "|| Apache Maven: $mavenVersion"
-    //     println "|| Java version: $javaVersion"
-    //     println "||=================================||"
-    // }
 }
--- vars/showMavenVersion.groovy
+++ vars/showMavenVersion.groovy
@@ -1,16 +1,13 @@
 import vn.com.example.VersionUtils
+import vn.com.example.utils.logging.LogLevel
+import vn.com.example.utils.logging.Logger
+import static vn.com.example.utils.ConfigConstants.*
+
 def call() {
-    println('Retrieving Maven information')
-    def (mavenVersion, javaVersion) = VersionUtils.getMavenAndJavaVersion(this)
-    def result = """
-||==========MAVEN VERSION==========||
-|| Apache Maven: ${mavenVersion}
-|| Java version: ${javaVersion}
-||=================================||
-"""
+    Logger.init(this, [ (LOGLEVEL) : LogLevel.WARN] )
+    Logger log = new Logger(this)
+    log.info('Retrieving Maven information')
+    
+    result = VersionUtils.getMavenAndJavaVersion(this)
     println(result)
-    // println "||==========MAVEN VERSION==========||"
-    // println "|| Apache Maven: $mavenVersion"
-    // println "|| Java version: $javaVersion"
-    // println "||=================================||"
 }
